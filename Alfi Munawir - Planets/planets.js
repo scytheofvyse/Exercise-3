51645ead0513d5f8dab81360dc3cc6010e6c05f2
scytheofvyse <31118364+scytheofvyse@users.noreply.github.com>
2019-03-21 12:03:01 +0700

    Delete planets.js

--- Alfi Munawir - Planets/planets.js
+++
@@ -1,9 +0,0 @@
-$(document).ready( function () {
-    var table = $('#example').DataTable( {
-      ajax: 'planets.json',
-      processing: true
-    } );
-})
-$('#reload').click( function () {
-    table.ajax.reload();
-  } );
